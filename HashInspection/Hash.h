04d701715e9bab71fff0f6a5b4e9b99349da8da6
metrovian <paramedoct@gmail.com>
2025-04-06 13:36:39 +0900

    SHA-512 Hash 구현

--- HashInspection/Hash.h
+++ HashInspection/Hash.h
@@ -4,6 +4,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <iomanip>
 #include <fstream>
 
 class Hash
--- HashInspection/main.cpp
+++ HashInspection/main.cpp
@@ -3,10 +3,11 @@
 #include "FibonacciHash.h"
 #include "MD5.h"
 #include "SHA256.h"
+#include "SHA512.h"
 
 int main()
 {
-	Hash* engine = new SHA256();
+	Hash* engine = new SHA512();
 
 	std::vector<uint8_t> hash1 = engine->str_hash("x64/HashInspection.exe");
 	std::vector<uint8_t> hash2 = engine->file_hash("x64/HashInspection.exe");
@@ -14,7 +15,7 @@ int main()
 	for (uint64_t i = 0; i < hash1.size(); ++i)
 	{
 		std::cout 
-			<< std::hex 
+			<< std::hex << std::setw(2) << std::setfill('0')
 			<< static_cast<uint64_t>(hash1[i]);
 	}
 	std::cout << std::endl;
@@ -22,7 +23,7 @@ int main()
 	for (uint64_t i = 0; i < hash2.size(); ++i)
 	{
 		std::cout
-			<< std::hex
+			<< std::hex << std::setw(2) << std::setfill('0')
 			<< static_cast<uint64_t>(hash2[i]);
 	}
 	std::cout << std::endl;
