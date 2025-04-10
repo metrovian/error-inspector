9bbb3a526d2224de81d6ba2ed7aaeb3cecfb20ad
metrovian <paramedoct@gmail.com>
2025-04-10 22:58:09 +0900

    Expander Code 구현

--- BlockInspection/main.cpp
+++ BlockInspection/main.cpp
@@ -1,17 +1,18 @@
 #include "Block.h"
 #include "Hamming74.h"
 #include "Hadamard32.h"
+#include "Expander84.h"
 
 int main()
 {
-	Block* engine = new Hadamard32();
+	Block* engine = new Expander84();
 
 	std::vector<uint8_t> raw = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x10 };
 
 	engine->get_raw() = raw;
 	engine->encode();
 
-	engine->get_redc().front() ^= (1 << 6);
+	engine->get_redc().front() ^= (1 << 2);
 	engine->decode();
 
 	for (uint64_t i = 0; i < raw.size(); i++)
